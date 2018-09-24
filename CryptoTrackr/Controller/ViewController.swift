3f1e3c9129c16b4de84c5276580f52c5d7de37c4
Hector Mendoza <hectormendoza@byui.edu>
2018-09-24 15:11:20 -0400

    UIPickerView source and delegate methods implemented

--- CryptoTrackr/Controller/ViewController.swift
+++ CryptoTrackr/Controller/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var cryptoPicker: UIPickerView!
+    
+    let crytoData = CryptoData()
+    var currencySymbol = ""
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/"
+    
+    var cryptoLabel = ""
+    var currencyLabel = ""
+    
+    var finalURL = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        cryptoPicker.delegate = self
+        cryptoPicker.dataSource = self
     }
-
-
 }
 
+
+
+
+//MARK: - UIPickerView Methods
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return crytoData.numComponents
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return crytoData.cryptoArray.count
+        } else if component == 1 {
+            return crytoData.currencyArray.count
+        } else {
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return crytoData.cryptoArray[row]
+        } else if component == 1 {
+            return crytoData.currencyArray[row]
+        } else {
+            return ""
+        }
+    }
+}
---
+++ CryptoTrackr/Model/CryptoData.swift
@@ -0,0 +1,18 @@
+//
+//  CryptoData.swift
+//  CryptoTrackr
+//
+//  Created by Hector Mendoza on 9/24/18.
+//  Copyright © 2018 Hector Mendoza. All rights reserved.
+//
+
+import Foundation
+
+class CryptoData {
+    let cryptoArray = ["", "BTC", "LTC", "ETH", "ZEC", "XRP", "XMR"]
+    
+    let currencyArray = ["", "AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySymbolArray = ["", "$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    
+    let numComponents = 2
+}
